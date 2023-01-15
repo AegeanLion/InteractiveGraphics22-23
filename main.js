e97bc71951810f90a3ca2f0901287d5df82923b0
AegeanLion <36489716+AegeanLion@users.noreply.github.com>
2023-01-15 18:54:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
 
 const objs = [];
 
-let sketch = function(p) {
-  p.setup = function() {
-    p.createCanvas(500, 500);
+function setup() {
+  createCanvas(500, 500);
 
 // initializing all pieces of barrel
  barrelBase1 = new barrelBase(0, 0, 0, 60/2, 50/2, 60/2);
@@ -32,28 +31,28 @@ let sketch = function(p) {
 //     console.log(objs[i]);
 //   }
 
-  }
-  
-  p.draw = function() {
-      // moving center points, setting angle mode etc
+}
+
+function draw() {
+  // moving center points, setting angle mode etc
   angleMode(DEGREES);
   background(220);
   translate(250, 250);
 
 //fills in the ocean with triangle shading
-  p.fill(ocean.fillColor);
-  p.triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[2][0], ocean.nodes[2][1]);
-  p.triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[4][0], ocean.nodes[4][1]);
-  p.triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[4][0], ocean.nodes[4][1]);
-  p.triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[2][0], ocean.nodes[2][1]);
-  p.triangle(ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[2][0], ocean.nodes[2][1]);
-  p.triangle(ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[6][0], ocean.nodes[6][1]);
-  p.triangle(ocean.nodes[6][0], ocean.nodes[6][1], ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[5][0], ocean.nodes[5][1]);
-  p.triangle(ocean.nodes[4][0], ocean.nodes[4][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[6][0], ocean.nodes[6][1]);
-  p.triangle(ocean.nodes[5][0], ocean.nodes[5][1], ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[6][0], ocean.nodes[6][1]);
-  p.triangle(ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[6][0], ocean.nodes[6][1]);
-  p.triangle(ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[4][0], ocean.nodes[4][1], ocean.nodes[6][0], ocean.nodes[6][1]);
-  p.noFill();
+  fill(ocean.fillColor);
+  triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[2][0], ocean.nodes[2][1]);
+  triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[4][0], ocean.nodes[4][1]);
+  triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[4][0], ocean.nodes[4][1]);
+  triangle(ocean.nodes[0][0], ocean.nodes[0][1], ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[2][0], ocean.nodes[2][1]);
+  triangle(ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[2][0], ocean.nodes[2][1]);
+  triangle(ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[6][0], ocean.nodes[6][1]);
+  triangle(ocean.nodes[6][0], ocean.nodes[6][1], ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[5][0], ocean.nodes[5][1]);
+  triangle(ocean.nodes[4][0], ocean.nodes[4][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[6][0], ocean.nodes[6][1]);
+  triangle(ocean.nodes[5][0], ocean.nodes[5][1], ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[6][0], ocean.nodes[6][1]);
+  triangle(ocean.nodes[7][0], ocean.nodes[7][1], ocean.nodes[1][0], ocean.nodes[1][1], ocean.nodes[6][0], ocean.nodes[6][1]);
+  triangle(ocean.nodes[3][0], ocean.nodes[3][1], ocean.nodes[4][0], ocean.nodes[4][1], ocean.nodes[6][0], ocean.nodes[6][1]);
+  noFill();
 
 
 // drawing all pieces of barrel + any other objects (automatically does it without object methods)
@@ -61,16 +60,13 @@ let sketch = function(p) {
     drawObj(objs[i]);
   }
 
-    //for rotating the objects
- p.mouseDragged = function() {
+}
+
+
+//for rotating the objects
+function mouseDragged() {
   for(let i = 0; i< objs.length; i++) {
     rotateX3D(objs[i], pmouseY - mouseY);
     rotateY3D(objs[i], pmouseX - mouseX);
   }
-  }
 }
-}
-
-new p5(sketch, "box1"); 
-
-
